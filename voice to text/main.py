a31258361ab89b23a9451cc6756d7c730ed1877e
mesteranas <135442710+mesteranas@users.noreply.github.com>
2024-05-14 12:53:00 +0300

    14/05/2024

--- voice to text/main.py
+++ voice to text/main.py
@@ -2,15 +2,35 @@ import sys
 from custome_errors import *
 sys.excepthook = my_excepthook
 import winsound
-import threading
 import speech_recognition as sr
 from webbrowser import open as openLink
 import language
 import app
 import PyQt6.QtWidgets as qt
 import PyQt6.QtGui as qt1
-from PyQt6.QtCore import Qt
+import PyQt6.QtCore as qt2
 language.init_translation()
+class Thread(qt2.QThread):
+    finish=qt2.pyqtSignal(str)
+    def __init__(self,p,languageCode):
+        super().__init__(p)
+        self.languageCode=languageCode
+    def run(self):
+        recognizer = sr.Recognizer()
+        with sr.Microphone() as source:
+            winsound.PlaySound("data/sounds/1.wav",1)
+            audio = recognizer.listen(source)
+        winsound.PlaySound("data/sounds/2.wav",1)
+        a=""
+        try:
+            a= recognizer.recognize_google(audio,language=self.languageCode)
+        except sr.UnknownValueError:
+            a="Sorry, could not understand audio."
+        except sr.RequestError as e:
+            a="Error with the API request: {0}".format(e)
+        self.finish.emit(a)
+
+
 class main (qt.QMainWindow):
     def __init__(self):
         super().__init__()
@@ -46,33 +66,17 @@ class main (qt.QMainWindow):
         help.addAction(about)
         about.triggered.connect(lambda:qt.QMessageBox.information(self,_("about"),_("{} version: {} description: {} developer: {}").format(app.name,str(app.version),app.description,app.creater)))
         self.setMenuBar(mb)
-    def ress(self,audioData):
-        recognizer = sr.Recognizer()
-        a=""
-        try:
-            a= recognizer.recognize_google(audioData, language=self.lang.text())
-        except sr.UnknownValueError:
-            a="Sorry, could not understand audio."
-        except sr.RequestError as e:
-            a="Error with the API request: {0}".format(e)
-        self.re.setText(a)
-    def res1(self,audioData):
-        t=threading.Thread(target=self.ress(audioData))
-        t.start()
-    def sayf(self):
-        recognizer = sr.Recognizer()
-        with sr.Microphone() as source:
-            winsound.PlaySound("data/sounds/1.wav",1)
-            recognizer.adjust_for_ambient_noise(source)  # Adjust for ambient noise
-            audio = recognizer.listen(source)  # Listen to the microphone input
-        winsound.PlaySound("data/sounds/2.wav",1)
-        self.res1(audio)
+    def result(self,text):
+        self.re.setText(text)
         self.re.setFocus()
     def sayf1(self):
-        threading.Thread(target=self.sayf()).start()
+        thread=Thread(self,self.lang.text())
+        thread.finish.connect(self.result)
+        thread.start()
 
 
-App=qt.QApplication([])
+
+App=qt.QApplication(sys.argv)
 w=main()
 w.show()
 App.exec()
